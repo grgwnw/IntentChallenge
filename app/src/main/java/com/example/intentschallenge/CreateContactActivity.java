54ce35c4c0113c92ac0352e6505d9d1ee8acfa9f
Eric Wang <46853724+grgwnw@users.noreply.github.com>
2021-01-11 14:21:02 -0500

    Upload code

---
+++ app/src/main/java/com/example/intentschallenge/CreateContactActivity.java
@@ -0,0 +1,100 @@
+package com.example.intentschallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CreateContactActivity extends AppCompatActivity {
+    EditText name, number, website, location;
+    ImageView happy,meh,sad;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_contact);
+        name = findViewById(R.id.name);
+        number = findViewById(R.id.number);
+        website = findViewById(R.id.website);
+        location = findViewById(R.id.location);
+        happy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isAcceptableOutput())
+                    sendResult(name.getText().toString().trim(),
+                            getString(R.string.happy),
+                            number.getText().toString().trim(),
+                            website.getText().toString().trim(),
+                            location.getText().toString().trim());
+                else{
+                    Toast.makeText(CreateContactActivity.this,"Invalid input. Please try again", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+        meh.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(isAcceptableOutput())
+                    sendResult(name.getText().toString().trim(),
+                            getString(R.string.meh),
+                            number.getText().toString().trim(),
+                            website.getText().toString().trim(),
+                            location.getText().toString().trim());
+                else{
+                    Toast.makeText(CreateContactActivity.this,"Invalid input. Please try again", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+        sad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isAcceptableOutput())
+                    sendResult(name.getText().toString().trim(),
+                            getString(R.string.sad),
+                            number.getText().toString().trim(),
+                            website.getText().toString().trim(),
+                            location.getText().toString().trim());
+                else{
+                    Toast.makeText(CreateContactActivity.this,"Invalid input. Please try again", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+    private void sendResult(String name, String emotion, String phone, String web, String pin){
+        String[] result = {name,emotion,phone,web,pin};
+        setResult(RESULT_OK,(new Intent()).putExtra("data",result));
+        CreateContactActivity.this.finish();
+    }
+    private boolean isAcceptableOutput(){
+        return isAcceptableName(name.getText().toString().trim())
+                && isAcceptableNumber(number.getText().toString().trim())
+                && isAcceptableWebsite(website.getText().toString().trim());
+        //Not sure how to check location
+    }
+    private boolean isAcceptableWebsite(String name){
+        if(name.length() < 4)
+            return false;
+        String domainName = name.substring(name.length() - 4);
+        return domainName == ".com" || domainName != ".org"
+                || domainName != ".net" || domainName != ".edu"
+                || domainName != ".gov";
+    }
+    private boolean isAcceptableName(String name){
+        return (name == null)? false : name.split(" ").length == 2;
+    }
+    private boolean isAcceptableNumber(String numbers){
+        if(numbers == null)
+            return false;
+        if(numbers.length() != 10) //If it is not the length of a telephone number
+            return false;
+        for(char number : numbers.toCharArray()){
+            int specificNumber = (int)number; //ASCII code of character
+            if(specificNumber < 48 || specificNumber > 57) //if it is outside range of numbers
+                return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/intentschallenge/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.intentschallenge;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button createContact;
+    ImageView emotion, phone, web, pin;
+    private String[] data; //Contains 4 indexes for the 4 pieces of information: web, location, call, and emotion
+    public static final int insertInfoActivityCode = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createContact = findViewById(R.id.createContact);
+        emotion = findViewById(R.id.emotion);
+        phone = findViewById(R.id.phone);
+        web = findViewById(R.id.web);
+        pin = findViewById(R.id.pin);
+        createContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        com.example.intentschallenge.CreateContactActivity.class);
+                startActivityForResult(intent,insertInfoActivityCode);
+            }
+        });
+        phone.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + data[2])));
+            }
+        });
+        web.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(data[3])));
+            }
+        });
+        pin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + data[4])));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == insertInfoActivityCode){
+
+            //If the data is invalid or other activity was cancelled
+            if(resultCode == RESULT_CANCELED || data.getStringArrayExtra("data").length != 5){return;}
+
+            //Else set the data equal (it was checked by CreateContactActivity.java before sent
+            this.data = data.getStringArrayExtra("data");
+            //Change emotion based on what they selected in the previous create contact
+            if(this.data[1] == getString(R.string.sad))
+                emotion.setImageResource(R.drawable.sad);
+            if(this.data[1] == getString(R.string.meh))
+                emotion.setImageResource(R.drawable.meh);
+            if(this.data[1] == getString(R.string.happy))
+                emotion.setImageResource(R.drawable.happy);
+
+            //Make buttons visible
+            emotion.setVisibility(View.VISIBLE);
+            phone.setVisibility(View.VISIBLE);
+            web.setVisibility(View.VISIBLE);
+            pin.setVisibility(View.VISIBLE);
+        }
+    }
+}
